7afdbe8445640f5d07a0f8097df32a5ff7c4d2db
learnsheet7-lab <learnsheet7@gmail.com>
2026-03-11 17:49:05 +0530

    logic refined19

--- server/index.js
+++ server/index.js
@@ -547,7 +547,7 @@ app.post('/fms/sync-dibiaa', async (req, res) => {
             else if (I !== 'Foil Print' && getAct(3)) {
                 plans[7] = addWorkdays(getAct(3), 3);
             } 
-            else if (getAct(6)) {
+            else if (F !== 'PVC Pad' && getAct(6)) {
                 plans[7] = addWorkdays(getAct(6), 3);
 
             }
