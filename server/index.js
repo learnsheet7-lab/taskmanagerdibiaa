646a77041b1138cdebe5f5181d2d0ebc70c71009
learnsheet7-lab <learnsheet7@gmail.com>
2026-03-12 15:55:38 +0530

    logic refined30

--- server/index.js
+++ server/index.js
@@ -489,7 +489,7 @@ app.post('/fms/sync-dibiaa', async (req, res) => {
             const N = parseDate(r[13]);
 
             const hasInner = K && K.toLowerCase().includes('inner print') || K.toLowerCase().includes('inner screen print');
-            const hasReadystock = K && K.toLowerCase().includes('ready stock') || K.toLowerCase().includes('ready to stock');
+            const hasReadystock = K || K!=='' && K.toLowerCase().includes('ready stock') || K.toLowerCase().includes('ready to stock');
             
             const isOffsetFoil = (I === 'Offset Print' || I === 'Foil Print' || I === 'No');
             const isScreenPrint = (I === 'Screen print');
@@ -560,8 +560,8 @@ app.post('/fms/sync-dibiaa', async (req, res) => {
         }
 
             // step 8 - full kitting
-            if(!hasReadystock){
-            if (F !== 'PVC Pad') {
+        
+            if (F !== 'PVC Pad' || !hasReadystock) {
                 if (F === 'Paper Bag' && I === 'Foil Print' && getAct(6)) {
                     plans[8] = addWorkdays(getAct(6), 3);
                 }
@@ -569,7 +569,7 @@ app.post('/fms/sync-dibiaa', async (req, res) => {
                     plans[8] = addWorkdays(getAct(7), 1);
                 }
             }
-        }
+        
 
             // if (getAct(8)) {
             //     const condition = (G === 'Magnetic' || (G || '').startsWith('Sliding Handle') && I === 'Screen print') || (G === 'Magnetic' && isOffsetFoil && hasInner) || (G === 'Magnetic' && hasInner && I === 'Screen print');
