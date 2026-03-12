702fc4cbea06d7b85d0e15c6085f1919f083d6d4
learnsheet7-lab <learnsheet7@gmail.com>
2026-03-12 16:51:43 +0530

    logic refined32

--- server/index.js
+++ server/index.js
@@ -489,7 +489,7 @@ app.post('/fms/sync-dibiaa', async (req, res) => {
             const N = parseDate(r[13]);
 
             const hasInner = K && K.toLowerCase().includes('inner print') || K.toLowerCase().includes('inner screen print');
-            const hasReadystock = K || K!=='' && K.toLowerCase().includes('ready stock') || K.toLowerCase().includes('ready to stock');
+            const hasReadystock = K && K.toLowerCase().includes('ready stock') || K.toLowerCase().includes('ready to stock');
             
             const isOffsetFoil = (I === 'Offset Print' || I === 'Foil Print' || I === 'No');
             const isScreenPrint = (I === 'Screen print');
@@ -560,7 +560,7 @@ app.post('/fms/sync-dibiaa', async (req, res) => {
         }
 
             // step 8 - full kitting
-        
+            if(!hasReadystock){
             if (F !== 'PVC Pad') {
                 if (F === 'Paper Bag' && I === 'Foil Print' && getAct(6)) {
                     plans[8] = addWorkdays(getAct(6), 3);
@@ -569,7 +569,7 @@ app.post('/fms/sync-dibiaa', async (req, res) => {
                     plans[8] = addWorkdays(getAct(7), 1);
                 }
             }
-        
+        }
 
             // if (getAct(8)) {
             //     const condition = (G === 'Magnetic' || (G || '').startsWith('Sliding Handle') && I === 'Screen print') || (G === 'Magnetic' && isOffsetFoil && hasInner) || (G === 'Magnetic' && hasInner && I === 'Screen print');
@@ -888,7 +888,7 @@ const performFmsSync = async () => {
 
         const A = parseDate(r[0]); const B = r[1]; const F = r[5]; const G = r[6]; const I = r[8]; const K = r[10]; const N = parseDate(r[13]);
         const hasInner = K && K.toLowerCase().includes('inner print') || K.toLowerCase().includes('inner screen print');
-            const hasReadystock = K || K!=='' && K.toLowerCase().includes('ready stock') || K.toLowerCase().includes('ready to stock');
+            const hasReadystock = K && K.toLowerCase().includes('ready stock') || K.toLowerCase().includes('ready to stock');
             
             const isOffsetFoil = (I === 'Offset Print' || I === 'Foil Print' || I === 'No');
             const isScreenPrint = (I === 'Screen print');
@@ -959,7 +959,7 @@ const performFmsSync = async () => {
         }
 
             // step 8 - full kitting
-          
+            if(!hasReadystock){
             if (F !== 'PVC Pad') {
                 if (F === 'Paper Bag' && I === 'Foil Print' && getAct(6)) {
                     plans[8] = addWorkdays(getAct(6), 3);
@@ -968,7 +968,7 @@ const performFmsSync = async () => {
                     plans[8] = addWorkdays(getAct(7), 1);
                 }
             }
-        
+        }
 
             // if (getAct(8)) {
             //     const condition = (G === 'Magnetic' || (G || '').startsWith('Sliding Handle') && I === 'Screen print') || (G === 'Magnetic' && isOffsetFoil && hasInner) || (G === 'Magnetic' && hasInner && I === 'Screen print');
